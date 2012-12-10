f444e7ce37a5efb1234b2ee0eef1360533931d01
Jonathan Wight <jwight@mac.com>
2012-12-10 10:16:43 -0800

    Initial Commit

---
+++ CocoaYAML/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CocoaYAML
+//
+//  Created by Jonathan Wight on 12/10/12.
+//  Copyright (c) 2012 toxicsoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
