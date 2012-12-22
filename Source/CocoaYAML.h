cbe7381fc9a8dd60e2acf6432fdc6fa16d215f8a
Jonathan Wight <jwight@mac.com>
2012-12-21 16:40:11 -0800

    Made OSX framework + reorg.

---
+++ Source/CocoaYAML.h
@@ -0,0 +1,9 @@
+//
+//  CYAMLDeserializer.h
+//  LayoutTest
+//
+//  Created by Jonathan Wight on 12/8/12.
+//  Copyright (c) 2012 toxicsoftware. All rights reserved.
+//
+
+#import <CocoaYAML/CYAMLDeserializer.h>
